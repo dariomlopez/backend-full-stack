7a9acf680b1bb1f6dfbceefadd6389788161e0d5
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-10-18 17:55:26 +0200

    Cambiando require en el exerciseModel

--- src/models/exerciseModel.js
+++ src/models/exerciseModel.js
@@ -1,4 +1,4 @@
-const database = require("../database/initDatabase");
+const database = require("../database/databaseConfig");
 
 const Exercise = {
   /** query para obtener todos los ejercicios */
