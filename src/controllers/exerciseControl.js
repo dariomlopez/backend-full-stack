d88f82c53c2c00b321d662faccfbae57494b45f1
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-10-19 17:47:05 +0200

    Terminando controlador lado cliente

--- src/controllers/exerciseControl.js
+++ src/controllers/exerciseControl.js
@@ -1,11 +1,11 @@
-const exerciseModel = require("../models/exerciseModel");
+const Exercise = require("../models/exerciseModel");
 const { solucionChatGPT, pistaChatGPT } = require("../utils/prompsGPT");
 
 const exerciseController = {
   /** Obtener todos los ejercicios */
   getAllExercises: async (request, response) => {
     try {
-      const exercises = await exerciseModel.getAllExercises(request.params.id);
+      const exercises = await Exercise.getAllExercises(request.params.id);
       response.json(exercises);
     } catch (error) {
       response.status(500).json({ error: "Erorr del servidor" });
@@ -16,13 +16,46 @@ const exerciseController = {
   getExerciseById: async (request, response) => {
     const { id } = request.params;
     try {
-      const exercise = await exerciseModel.getExerciseById(id);
+      const exercise = await Exercise.getExerciseById(id);
 
       if (!exercise) {
         return response.status(404).json({ error: "Error del servidor" });
       }
-    }catch (error) {
+    } catch (error) {
       response.status(500).json({ error: "Erorr del servidor" });
     }
   },
-}
+
+  /** crear nuevo ejercicio */
+  createNewExercise: async (request, response) => {
+    const { titulo, explicacion, tags } = request.body;
+    try {
+      const newExercise = await Exercise.createExercise(titulo, explicacion, tags);
+      response.json(newExercise);
+    } catch (error) {
+      response.status(500).json({ error: "Error del servidor" });
+    }
+  },
+
+  /** Solución y pistas para un ejercicio por ID */
+  exerciseHelpById: async (request, response) => {
+    try {
+      const ejercise = await Exercise.getExerciseById(request.params.id);
+
+      if(!ejercicio) {
+        return response.status(404).send("Ejercicio no encontrado")
+      }
+
+      const solucion = await solucionChatGPT(ejercise.explicacion);
+      const pistas = await pistaChatGPT(ejercise.explicacion);
+
+      response.json({ ejercise, solucion, pistas });
+
+    } catch (error) {
+      console.error("Error al obtener ejercicio", error);
+      response.status(500).send("Error del servidor");
+    }
+  },
+};
+
+module.exports = exerciseController;
