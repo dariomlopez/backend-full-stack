d21e7d2f76f4e193ee0ef824493edab1d134b771
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-10-19 15:10:59 +0200

    Añadiendo rutas get y post a server.js

--- server.js
+++ server.js
@@ -21,9 +21,55 @@ app.get("/ejercicios", async (request, response) => {
     const ejercicios = await Exercise.getAllExercises();
     response.json(ejercicios);
   } catch (error) {
-    console.error("Error a obtener ejercicios:", error);
+    console.error("Error al obtener ejercicios:", error);
     response.status(500).send("Error del servidor");
   }
 });
 
 /** Obtener ejercicio por ID */
+app.get("/ejercicios/:id", async (request, response) => {
+  try {
+    const ejercicio = await Exercise.getExerciseById(request.params.id);
+
+    if (!ejercicio) {
+      return response.status(404).send("Ejercicio no encontrado");
+    }
+
+    response.json(ejercicio);
+  } catch(error) {
+    console.error("Error al obtener ejercicios:", error);
+    response.status(500).send("Error del servidor");
+  }
+});
+
+/** Crear un ejercicio */
+app.post("ejercicios", async (request, response) => {
+  try {
+    const { titulo, explicacion, tags, pistas } = request.body;
+    const nuevoEjercicio = await Exercise.createExercise(titulo, explicacion, tags, pistas);
+  } catch (error) {
+    console.error("Error al obtener ejercicios:", error);
+    response.status(500).send("Error del servidor");
+  }
+});
+
+/** Obtener pista por id de ejercicio */
+app.get("ejercicios/:id/pista", async (request, response) => {
+  try {
+    const pista = await Exercise.clueByExerciseId(request.params.id);
+
+    if (!pista) {
+      return response.status(404).send("Pista no encontrada");
+    }
+
+    response.json(pista);
+  } catch (error) {
+    console.error("Error al obtener ejercicios:", error);
+    response.status(500).send("Error del servidor");
+  }
+});
+
+/** Iniciar el servidor */
+app.listen(port, () => {
+  console.log(`Servidor funcionando en http://localhost:${port}`)
+})
