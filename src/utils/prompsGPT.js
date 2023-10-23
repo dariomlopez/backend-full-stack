1af478b5d464b3ab700a0715aa6fa7379e3b4b59
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-10-23 16:12:01 +0200

    APIKEY

--- src/utils/prompsGPT.js
+++ src/utils/prompsGPT.js
@@ -9,7 +9,7 @@ const apiKey = process.env.OPENAI_API_KEY;
 const respuestaChatGPT = async (input) => {
   try {
     /** Sustituir por API */
-    const response = await axios.post("https://api.openai.com/v1/completions", 
+    const response = await axios.post(apiKey, 
     {
       prompt: input,
       max_tokens: 2000,
